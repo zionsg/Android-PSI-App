2f6dea1a62568ad26530069dea2b9728ae15f8b3
Zion Ng <zion@intzone.com>
2017-10-23 23:36:04 +0800

    Do simple HTTP request and dialog

--- app/src/main/java/sg/zion/androidpsiapp/MapsActivity.java
+++ app/src/main/java/sg/zion/androidpsiapp/MapsActivity.java
@@ -1,15 +1,27 @@
 package sg.zion.androidpsiapp;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+
+import org.json.JSONObject;
 
 public class MapsActivity extends FragmentActivity
         implements OnMyLocationButtonClickListener,
@@ -52,8 +64,43 @@ public class MapsActivity extends FragmentActivity
 
     @Override
     public boolean onMyLocationButtonClick() {
+        callApi();
+
         // Return false so that we don't consume the event and the default behavior still occurs
         // (the camera animates to the user's current position).
         return false;
+    }
+
+    protected void callApi() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://jsonplaceholder.typicode.com/posts/1";
+
+        // Request a JSON response from the provided URL.
+        JsonObjectRequest jsonRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        showDialog("callApi", "Response: " + response.toString());
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        showDialog("callApi", "Error: " + error.toString());
+                    }
+                });
+
+        // Add the request to the RequestQueue.
+        queue.add(jsonRequest);
+    }
+
+    protected void showDialog(String title, String message) {
+         new AlertDialog.Builder(this)
+                 .setTitle("Dialog")
+                 .setMessage(message)
+                 .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                     @Override
+                     public void onClick(DialogInterface dialog, int which) {
+                     }
+                 }).setNegativeButton("Cancel", null).show();
     }
 }
